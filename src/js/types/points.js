9b67be1ef841a7e187cf15b50f155f7fc778ff20
muyuu <anticyborg@gmail.com>
2017-05-13 23:59:51 +0900

    add points type

---
+++ src/js/types/points.js
@@ -0,0 +1,15 @@
+// @flow
+
+export type PointAction = {
+    type: string,
+    id: string,
+    place: string,
+    arrivedAt: number,
+    duration: number,
+}
+export type PointObj = {
+    id: string,
+    place: string,
+    arrivedAt: number,
+    duration: number,
+}
