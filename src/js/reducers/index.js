7873dedbe95d1ee4647857640fd07a286a4515d8
muyuu <anticyborg@gmail.com>
2017-05-13 23:59:06 +0900

    use siteinfo

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,9 @@
 // @flow
 import {combineReducers} from 'redux';
+import {siteinfo} from './siteInfo';
 
 const reducers = combineReducers({
+    siteinfo,
 });
 
 export default reducers;
---
+++ src/js/reducers/siteInfo/index.js
@@ -0,0 +1,14 @@
+// @flow
+
+const initialSiteInfo = {
+    name: "brilliantJourney",
+};
+
+export const siteInfo = (state = initialSiteInfo, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+
