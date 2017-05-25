a265f03ff442becfd4f939fde73a49bcc0211866
muyuu <anticyborg@gmail.com>
2017-05-25 13:20:02 +0900

    tweak

--- src/js/stateTest.js
+++ src/js/stateTest.js
@@ -1,4 +1,4 @@
-import {addPoint, pointPlace, pointArrivedAt, pointDuration} from './actions/points';
+import {addPoint, pointPlace, pointArrivalTime, pointDuration} from './actions/points';
 
 /* eslint-disable */
 module.exports = (store) => {
@@ -10,27 +10,27 @@ module.exports = (store) => {
     store.dispatch(addPoint());
     const startId = store.getState().points[0].id;
     store.dispatch(pointPlace(startId, 'Tokyo'));
-    store.dispatch(pointArrivedAt(startId, parseInt(new Date('2017/5/1 09:00') / 1000)));
+    store.dispatch(pointArrivalTime(startId, parseInt(new Date('2017/5/1 09:00') / 1000)));
 
     // set 1st point
     store.dispatch(addPoint());
     const firstId = store.getState().points[1].id;
     store.dispatch(pointPlace(firstId, 'Saitama'));
-    store.dispatch(pointArrivedAt(firstId, parseInt(new Date('2017/5/1 12:00') / 1000)));
+    store.dispatch(pointArrivalTime(firstId, parseInt(new Date('2017/5/1 12:00') / 1000)));
     store.dispatch(pointDuration(firstId, 123456));
 
     // set 2nd point
     store.dispatch(addPoint());
     const secondId = store.getState().points[2].id;
     store.dispatch(pointPlace(secondId, 'Gumma'));
-    store.dispatch(pointArrivedAt(secondId, parseInt(new Date('2017/5/1 18:00') / 1000)));
+    store.dispatch(pointArrivalTime(secondId, parseInt(new Date('2017/5/1 18:00') / 1000)));
     store.dispatch(pointDuration(secondId, 123456));
 
     // set 3rd point
     store.dispatch(addPoint());
     const endId = store.getState().points[3].id;
     store.dispatch(pointPlace(endId, 'Tokyo'));
-    store.dispatch(pointArrivedAt(endId, parseInt(new Date('2017/5/1 22:00') / 1000)));
+    store.dispatch(pointArrivalTime(endId, parseInt(new Date('2017/5/1 22:00') / 1000)));
 
     unsubscribe();
 };
