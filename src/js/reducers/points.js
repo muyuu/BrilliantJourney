e9b93432259105e54faf823c1c4b9da6492f0aa6
muyuu <anticyborg@gmail.com>
2017-05-25 13:47:03 +0900

    divide reducer point

--- src/js/reducers/points.js
+++ src/js/reducers/points.js
@@ -7,7 +7,7 @@ import {
     RECEIVE_POINT,
     CLEAR_SUGGESTED_TEXT
 } from "../actions/actionTypes";
-import  {suggestPoint} from "./suggestPoint";
+import {point} from './point';
 
 export const points = (state = [], action) =>{
     switch(action.type){
@@ -28,46 +28,6 @@ export const points = (state = [], action) =>{
 
                 return point(v, action);
             });
-
-
-        default:
-            return state;
-    }
-};
-
-const point = (state, action) =>{
-    switch(action.type){
-        case ADD_POINT_INPUT:
-            return {
-                id: action.id,
-                name: '',
-                placeId: '',
-                arrivalTime: 0,
-                duration: 0,
-                suggested: {
-                    isFetching: false,
-                    items: [],
-                    lastUpdated: 0,
-                },
-            };
-
-        case SET_POINT_PLACE:
-            return Object.assign({}, state, {
-                name: action.name,
-                placeId: action.placeId,
-            });
-
-        case SET_POINT_ARRIVAL_TIME:
-            return Object.assign({}, state, {arrivalTime: action.arrivalTime});
-
-        case SET_POINT_DURATION:
-            return Object.assign({}, state, {duration: action.duration});
-
-        case REQUEST_POINT:
-        case RECEIVE_POINT:
-        case CLEAR_SUGGESTED_TEXT:
-            return Object.assign({}, state, {suggested: suggestPoint(state.suggested, action)});
-
 
         default:
             return state;
