4a5af14767ae1783d44dc82d1982f938c2239e18
muyuu <anticyborg@gmail.com>
2017-09-21 10:02:49 +0900

    tweak for components

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,24 +1,27 @@
 // @flow
 import React from 'react';
-
 import Header from '../containers/layouts/Header';
 import Body from '../containers/layouts/Body';
 import Footer from '../containers/layouts/Footer';
 import styles from './app.css';
 
-const App = () => {
-    return (
-        <div className="page">
-            <Header />  
-
-            <div className="body">
-                <div id="map"></div>
-                <Body className={styles.wrapper} />
+const App = () => (
+    <div className={styles.wrapper}>
+        <div className={styles.header}>
+            <Header />
+        </div>
+        
+        <div className={styles.body}>
+            <div id="map" className={styles.map}></div>
+            <div className={styles.content}>
+                <Body />
             </div>
-
+        </div>
+        
+        <div className={styles.footer}>
             <Footer />
         </div>
-    );
-};
+    </div>
+);
 
 export default App;
--- src/layouts/Body.js
+++ src/layouts/Body.js
@@ -1,16 +1,13 @@
 // @flow
 import React from 'react';
-
 import Start from '../containers/Start';
 import Points from '../containers/Points';
 
-const Body = (props: {siteInfo: {name: string}}) => {
-    return (
-        <div className="body">
-            <Start />
-            <Points />
-        </div>
-    );
-};
+const Body = () => (
+    <div className="body">
+        <Start />
+        <Points />
+    </div>
+);
 
 export default Body;
