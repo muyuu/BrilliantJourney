13df3df335fac72b0ad03d52d932bb29d9e3d078
muyuu <anticyborg@gmail.com>
2017-09-22 23:36:18 +0900

    refactor start component

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import {render} from 'react-dom';
 import SuggestPoint from '../containers/SuggestPoint';
 
-class Start extends React.Component {
-    constructor(props){
-        super(props);
-    }
+const Start = ({
+    start,
+    onChangeDepartureTime,
+}) => {
+    const onChangeDepartureTimeHandler = ({ target }) => {
+        const id = start.id;
+        const departureTime = target.value;
+        onChangeDepartureTime(id, departureTime);
+    };
 
-    onChangeDepartureTimeHandler(e){
-        const id = this.props.start.id;
-        const departureTime = e.target.value;
-        this.props.onChangeDepartureTime(id, departureTime);
-    }
-
-    render(){
-        return (
-            <div className="point">
-                <div className="point__place">
-                    <label>
-                        <span>スタート</span>
-                        <SuggestPoint point={this.props.start} />
-                    </label>
-                </div>
-                <div className="point__arriveAt">
-                    <label>
-                        <span>出発時間</span>
-                        <input type="datetime-local" onChange={this.onChangeDepartureTimeHandler.bind(this)}/>
-                    </label>
-                </div>
+    return (
+        <div className="point">
+            <div className="point__place">
+                <label>
+                    <span>スタート</span>
+                    <SuggestPoint point={start} />
+                </label>
             </div>
-        );
-    }
-}
+            <div className="point__arriveAt">
+                <label>
+                    <span>出発時間</span>
+                    <input type="datetime-local" onChange={onChangeDepartureTimeHandler}/>
+                </label>
+            </div>
+        </div>
+    );
+};
 
 export default Start;
