2bb94d34aebfd25eedb05220999bdd2caef14d4c
muyuu <anticyborg@gmail.com>
2017-09-22 10:03:28 +0900

    refactor point component

--- src/components/Point.js
+++ src/components/Point.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import {render} from 'react-dom';
 import SuggestPoint from '../containers/SuggestPoint';
 
-class Point extends React.Component {
-    constructor(props){
-        super(props);
-    }
+const Point = ({
+    index,
+    point,
+    onChagenDuration,
+}) => {
+    const onChangeDurationHandler = ({ target: { value }}) => {
+        const id = point.id;
+        onChagenDuration(id, value);
+    };
 
-    onChangeDurationHandler(e){
-        const id = this.props.point.id;
-        const duration = e.target.value;
-        this.props.onChagenDuration(id, duration);
-    }
-
-    render(){
-        return (
-            <div className="point">
-                <div className="point__place">
-                    <label>
-                        <span>ポイント{this.props.index + 1}</span>
-                        <SuggestPoint point={this.props.point}/>
-                    </label>
-                </div>
-                <div className="point__duration">
-                    <label>
-                        <span>滞在時間</span>
-                        <input type="number" onChange={this.onChangeDurationHandler.bind(this)}/>
-                    </label>
-                </div>
+    return (
+        <div className="point">
+            <div className="point__place">
+                <label>
+                    <span>ポイント{index + 1}</span>
+                    <SuggestPoint point={point}/>
+                </label>
             </div>
-        );
-    }
-}
+            <div className="point__duration">
+                <label>
+                    <span>滞在時間</span>
+                    <input type="number" onChange={(e)=> onChangeDurationHandler(e)}/>
+                </label>
+            </div>
+        </div>
+    );
+};
 
 export default Point;
