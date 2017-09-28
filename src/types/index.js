d1237539935bfc702967ae38ed63692e7257230a
muyuu <anticyborg@gmail.com>
2017-09-28 09:51:55 +0900

    tweak types

--- src/types/index.js
+++ src/types/index.js
@@ -1,11 +1,12 @@
 // @flow
 import type { Store as ReduxStore, Dispatch as ReduxDispatch } from 'redux';
 
-import type {StartState} from './start';
-import type {PointAction, PointState} from './points';
+import type { SiteInfo } from './siteinfo';
+import type { StartState } from './start';
+import type { PointAction, PointState } from './points';
 
 export type ReduxInitAction = { type: '@@INIT' };
-export type State = StartState & PointState;
+export type State = StartState & PointState & SiteInfo;
 export type Action = ReduxInitAction | PointAction;
 export type Store = ReduxStore<State, Action>;
 export type Dispatch = ReduxDispatch<Action>;
