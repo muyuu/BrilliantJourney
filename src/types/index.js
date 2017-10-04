80ad6585a9e93422e953916afbaa55b224aeae4a
muyuu <anticyborg@gmail.com>
2017-10-04 10:39:57 +0900

    add Redux Types

--- src/types/index.js
+++ src/types/index.js
@@ -7,6 +7,11 @@ import type { PointAction, PointState } from './points';
 
 export type ReduxInitAction = { type: '@@INIT' };
 export type State = StartState & PointState & SiteInfo;
-export type Action = ReduxInitAction | PointAction;
+export type Action = 
+    | ReduxInitAction
+    | PointAction;
 export type Store = ReduxStore<State, Action>;
 export type Dispatch = ReduxDispatch<Action>;
+export type GetState = () => State;
+export type PromiseAction = Promise<Action>;
+export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
